5914d23ac6c1985397ead1642e70d7193d3d10c4
lschmittling <lanceschmittling@Lances-MBP>
2018-04-08 12:12:42 -0500

    Getting node to work with mysql

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -4,11 +4,37 @@ var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
     host: "localhost",
-    port: 3306,
     user: "root",
-    database: "bamazon"
 });
 
 connection.connect(function(err) {
     console.log("Connected as id: " + connection.threadId);
-}); 
+});
+
+var queryString = "SELECT * FROM bamazon.products";
+
+connection.query(queryString, function(err, rows, fields) {
+    if (err) throw err;
+ 
+    for (var i in rows) {
+        console.log("Product " + rows[i].id + ": " + rows[i].product_name);
+    }
+});
+ 
+connection.end();
+
+
+// var start = function() {
+//     inquirer.prompt({
+//         name: "postOrBid",
+//         type: "rawlist",
+//         message: "Would you like to [POST] or [BID]?",
+//         choices: ["POST", "BID"]
+//     }).then(function(answer){
+//         if (answer.postOrBid.toUpperCase() == "POST") {
+//             console.log("success");
+//         } else {
+//             console.log("failure");
+//         }
+//     })
+// }
