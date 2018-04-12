fc5ec7272912c6ef451b749e9750912e7fe37907
lschmittling <lanceschmittling@Lances-MBP>
2018-04-12 14:44:26 -0500

    Got functionality to work for inquirer. Now need to update mysql

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -19,19 +19,16 @@ connection.connect(function(err) {
 function readProducts() {
   connection.query("SELECT * FROM products", function(err, res) {
     if (err) throw err;
-    
+
+    // Using npm cli-table for table layout in console
     var table = new Table({
-        head: ["Item ID", "Product", "Price"],
-        colWidths: [10, 20, 10],
-        style: {
-            head: ["green"],
-        }
+      head: ["Item ID", "Product", "Price"],
+      colWidths: [10, 20, 10]
     });
 
+    // Loop through results and push them to the cli-table
     for (var i = 0; i < res.length; i++) {
-        table.push(
-            [res[i].item_id, res[i].product_name, res[i].price]
-        );
+      table.push([res[i].item_id, res[i].product_name, res[i].price]);
     }
     console.log(table.toString() + "\n");
     promptUser();
@@ -39,39 +36,34 @@ function readProducts() {
 }
 
 function promptUser() {
-    inquirer.prompt([
-        {
-            type: "input",
-            name: "id",
-            message: "What's the ID of the product you'd like to buy?"
-        },
-        {
-            type: "input",
-            name: "units",
-            message: "How many would you like to buy?"
-        }
-    ]).then(answers => {
-        connection.query("SELECT * FROM products WHERE item_id=?", ["answers.units"], function(err, res) {
-            if (err) throw err;
-            if (answers.units > res.stock_quantity) {
-                console.log("INSUFFICIENT QUANTITY!");
-                promptUser();
-            } else {
-                console.log(answers.id);
-                console.log(answers.units);
-                console.log("it worked");
-            }
-            connection.end();
-        });
+  inquirer
+    .prompt([
+    {
+        type: "input",
+        name: "itemID",
+        message: "What's the ID of the product you'd like to buy?"
+    },
+	{
+        type: "input",
+        name: "quantity",
+        message: "How many would you like to buy?"
+    }
+    ])
+    .then(function(answers) {
+    	connection.query("SELECT * FROM Products WHERE item_id= " + answers.itemID, function(err, res) {
+			if (err) throw err;
+			if (answers.quantity <= res[0].stock_quantity) {
+				var totalPrice = (parseInt(answers.quantity * res[0].price));
+				console.log("\n--------------------------------------------");
+				console.log("\nThank you for your purchase!\nYour total for " + answers.quantity + " items is $" + totalPrice + ".\n");
+				console.log("--------------------------------------------\n");
+				readProducts();
+			} else {
+				console.log("\n--------------------------------------------");
+				console.log("\nINSUFFICIENT QUANTITY!\n");
+				console.log("--------------------------------------------\n");
+				readProducts();
+			}
+		})
     });
 }
-
-
-// console.log("--------------------------------------------");
-    // console.log("ITEMS FOR SALE");
-    // console.log("--------------------------------------------\n");
-    // for (var i = 0; i < res.length; i++) {
-    //   console.log("ID: " + res[i].item_id + " | Item: " + res[i].product_name + " | Price: $" + res[i].price);
-    // }
-    // console.log("\n--------------------------------------------\n")
-    // promptUser();
