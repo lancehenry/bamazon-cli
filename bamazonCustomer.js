c0d73ab8e5468eabadc4d13b06908724b0b24f8d
lschmittling <lanceschmittling@Lances-MBP>
2018-04-13 10:00:44 -0500

    Got all the functions working

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -39,61 +39,67 @@ function readProducts() {
 function promptUser() {
   inquirer
     .prompt([
-    {
+      {
         type: "input",
         name: "itemID",
         message: "What's the ID of the product you'd like to buy?"
-    },
-	{
+      },
+      {
         type: "input",
         name: "quantity",
         message: "How many would you like to buy?"
-    }
+      }
     ])
     .then(function(answers) {
-    	connection.query("SELECT * FROM products WHERE item_id= " + answers.itemID, function(err, res) {
-			if (err) throw err;
-			if (answers.quantity <= res[0].stock_quantity) {
+      connection.query(
+        "SELECT * FROM products WHERE item_id= " + answers.itemID,
+        function(err, res) {
+          if (err) throw err;
+          if (answers.quantity <= res[0].stock_quantity) {
+            // Calculates Total price for amount of items purchased
+            var totalPrice = parseInt(answers.quantity * res[0].price);
+            console.log("\n--------------------------------------------");
+            console.log("\nThank you for your purchase!\nYour total for " + answers.quantity + " items is $" + totalPrice + ".\n");
+            console.log("--------------------------------------------\n");
 
-        // Calculates Total price for amount of items purchased
-				var totalPrice = (parseInt(answers.quantity * res[0].price));
-				console.log("\n--------------------------------------------");
-				console.log("\nThank you for your purchase!\nYour total for " + answers.quantity + " items is $" + totalPrice + ".\n");
-        console.log("--------------------------------------------\n");
+            // Stored new quantity in a variable. This is the only way I got the query to update below.
+            var updateQuantity = res[0].stock_quantity - answers.quantity;
 
-        // Stored new quantity in a variable. This is the only way I got the query to update below.
-        var updateQuantity = (res[0].stock_quantity - answers.quantity);
-
-        // Updates MySQL database with entered quantities
-        connection.query("UPDATE products SET stock_quantity=? WHERE item_id=?", [updateQuantity, answers.itemID]);
-        donePurchasing();
-			} else {
-				console.log("\n--------------------------------------------");
-				console.log("\nINSUFFICIENT QUANTITY!\n");
-				console.log("--------------------------------------------\n");
-				readProducts();
-			}
-		})
-  });
+            // Updates MySQL database with entered quantities
+            connection.query(
+              "UPDATE products SET stock_quantity=? WHERE item_id=?",
+              [updateQuantity, answers.itemID]
+            );
+            donePurchasing();
+          } else {
+            console.log("\n--------------------------------------------");
+            console.log("\nINSUFFICIENT QUANTITY!\n");
+            console.log("--------------------------------------------\n");
+            readProducts();
+          }
+        }
+      );
+    });
 }
 
 function donePurchasing() {
-  inquirer.prompt([
-    {
-      type: "list",
-      name: "finished",
-      message: "Are you done purchasing?",
-      choices: ["Yes", "No"]
-    }
-  ])
-  .then(function(answers) {
-    if (answers.finished === "No") {
-      readProducts();
-    } else {
-      console.log("\n--------------------------------------------");
-			console.log("\nCOME BACK SOON!\n");
-			console.log("--------------------------------------------\n");
-      connection.end();
-    }
-  }) 
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "finished",
+        message: "Are you done purchasing?",
+        choices: ["Yes", "No"]
+      }
+    ])
+    .then(function(answers) {
+      if (answers.finished === "No") {
+        readProducts();
+      } else {
+        console.log("\n--------------------------------------------");
+        console.log("\nCOME BACK SOON!\n");
+        console.log("--------------------------------------------\n");
+        connection.end();
+      }
+    });
 }
